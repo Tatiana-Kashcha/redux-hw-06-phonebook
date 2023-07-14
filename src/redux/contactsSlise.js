a4e0c0e07677cf902debc1a6e4a15f7f14c2669c
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-14 18:37:54 +0300

    Update contactsSlise.js

--- src/redux/contactsSlise.js
+++ src/redux/contactsSlise.js
@@ -6,35 +6,68 @@ const initialState = {
 
 export const contactsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'account/deposit':
-      return {
-        ...state,
-        balance: state.balance + action.payload,
+    case 'contacts/addUser':
+      return data => {
+        if (
+          state.some(
+            contact => contact.name.toLowerCase() === data.name.toLowerCase()
+          )
+        ) {
+          alert('This name is already in the contacts list.');
+          return;
+        }
+        prev => [...prev, action.payload];
       };
 
+    case 'contacts/deleteUser':
+      return state.filter(el => el.id !== action.payload);
     default:
       return state;
   }
 };
 
+// export const addUser = data => {
+//   const newUser = {
+//     id: nanoid(),
+//     ...data,
+//   };
+
+// const isDuplicateUser = contacts.some(
+//   contact => contact.name.toLowerCase() === data.name.toLowerCase()
+// );
+
+// if (isDuplicateUser) {
+//   alert('This name is already in the contacts list.');
+//   return;
+// }
+
+// if (
+//   state.some(contact => contact.name.toLowerCase() === data.name.toLowerCase())
+// ) {
+//   alert('This name is already in the contacts list.');
+//   return;
+// }
+
+//   setContacts(prev => [...prev, newUser]);
+// };
+
 export const addUser = data => {
-  const newUser = {
-    id: nanoid(),
-    ...data,
+  return {
+    type: 'contacts/addUser',
+    payload: {
+      id: nanoid(),
+      ...data,
+    },
   };
-
-  const isDuplicateUser = contacts.some(
-    contact => contact.name.toLowerCase() === data.name.toLowerCase()
-  );
-
-  if (isDuplicateUser) {
-    alert('This name is already in the contacts list.');
-    return;
-  }
-
-  setContacts(prev => [...prev, newUser]);
 };
 
+// export const deleteUser = id => {
+//   setContacts(contacts.filter(el => el.id !== id));
+// };
+
 export const deleteUser = id => {
-  setContacts(contacts.filter(el => el.id !== id));
+  return {
+    type: 'contacts/deleteUser',
+    payload: id,
+  };
 };
