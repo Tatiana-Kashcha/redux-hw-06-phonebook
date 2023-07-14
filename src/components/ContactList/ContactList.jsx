e753cbbf2913a8bcb98d76395094c8bc15e724ef
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-15 00:56:00 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,15 +5,13 @@ import * as s from './ContactList.styled';
 export const ContactList = () => {
   const users = useSelector(state => state.contactsUser.contacts);
   const filter = useSelector(state => state.filterUser.filter);
-  console.log(filter);
 
-  // const searchUserBook = () => {
-  //   // const normalised = filter.toLowerCase();
-  //   return users.filter(contact =>
-  //     contact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  // };
-  const searchUser = users;
+  const searchUserBook = () => {
+    return users.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const searchUser = searchUserBook();
 
   return (
     <s.List>
