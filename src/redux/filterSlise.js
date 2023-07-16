0c48eeadd04a68af5877c0dccba0400569ecc52b
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-16 15:29:20 +0300

    Update filterSlise.js

--- src/redux/filterSlise.js
+++ src/redux/filterSlise.js
@@ -12,19 +12,6 @@ export const filterReducer = (state = initialState, action) => {
   }
 };
 
-// export const handleCangeFilter = e => {
-//   setFilter(e.target.value.trim());
-// };
-
-// export const handleCangeFilter = e => {
-//   return {
-//     type: 'filter/handleCangeFilter',
-//     payload: e,
-//   };
-// };
-// case 'filter/handleCangeFilter':
-//       return { ...state, filter: action.payload.target.value.trim()};
-
 export const handleCangeFilter = text => {
   return {
     type: 'filter/handleCangeFilter',
